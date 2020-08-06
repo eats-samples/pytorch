f14d48dfbd28173cb1de3628fb5504712b8f3ed8
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-06 16:30:33 -0700

    Vulkan resource cache.
    ghstack-source-id: 23314a71190b6695d7686a7cbec961f6af685b97
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42709

---
+++ aten/src/ATen/native/vulkan/api/Resource.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+#include <ATen/native/vulkan/api/Allocator.h>
+#include <ATen/native/vulkan/api/Cache.h>
+#include <c10/util/hash.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+struct Resource final {
+  struct Pool final {
+
+  };
+};
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
